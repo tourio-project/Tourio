aca247268d82b79a7b2af13103397b9b01a48f43
Polar-Goat <105109037+Polar-Goat@users.noreply.github.com>
2025-10-01 21:42:46 +0300

    Update GenData.py

--- AiService/GenData.py
+++ AiService/GenData.py
@@ -1,33 +1,40 @@
 import random
-import firebase_admin
-from firebase_admin import credentials,firestore
-cred = credentials.Certificate("new tourio key python.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
+if __name__ == "__main__":
+    import firebase_admin
+    from firebase_admin import firestore
+    if not firebase_admin._apps:
+        cred = firebase_admin.credentials.Certificate("new tourio key python.json")
+        firebase_admin.initialize_app(cred)
+    db = firestore.client()
 
 MOODS = ["Calm","Romantic","Adventurous","Spontaneous",
-         "Festive","Casual","Exploratory","Tired",
-         "Cozy","Hungry","Curious","Creative", "Bored"]
+            "Festive","Casual","Exploratory","Tired",
+            "Cozy","Hungry","Curious","Creative", "Bored"]
 WEATHERS = ["Sunny", "Hot", "Raining","Cloudy","Cold","Snow","Humid"]
 CAT = ["Food & Drink","Nature/Outdoors","Culture/History","Art",
-       "Adventure","Shopping","Spirituality","Festivals",
-       "Wellness","Local Life","Sports","Spa & Beauty"]
+        "Adventure","Shopping","Spirituality","Festivals",
+        "Wellness","Local Life","Sports","Spa & Beauty"]
 MAX_BUDGET = 300
 
 
-def generate_rand_location(id):
+def random_location(id):
     mood = random.sample(MOODS,random.randint(1,3))
-    weather = random.choice(WEATHERS)
+    weather = random.sample(WEATHERS,random.randint(1,3))
     cost = random.randint(20,250)
     categories = random.sample(CAT, random.randint(1,3))
     lat = random.uniform(29.3711572359193,32.41564584687414)
     lon = random.uniform(35.61400818573995,36.340664462085215)
     return {"name": id,"mood": mood, "weather_ok": weather, "cost": cost, "cat": categories,"lat":lat,"lon":lon}
 
-def generate_Locs(n):
+def generate_locs(n):
     for i in range(1,n+1):
-        loc = generate_rand_location(i)
-        db.collection("Locations").document(str(i)).set(loc)
+        loc = random_location(i)
+        db.collection("Training_Locations").document(str(i)).set(loc)
         print("Added location with ID: ",i)
 
+def random_user():
+    mood = random.sample(MOODS,random.randint(1,3))
+    weather = random.choice(WEATHERS)
+    budget = random.randint(20,250)
+    interests = random.sample(CAT, random.randint(1,3))
+    return {"mood": mood, "weather": weather, "budget": budget, "interests": interests}
